ec1ad0d3e00e4e47c213594005de4c934db73b44
rhanielfarias <rhanielfarias@gmail.com>
2022-10-18 19:02:25 -0300

    Criando a modelagem do projeto

--- src/main/java/com/catalisa/redesolidaria/repository/TipoDaDeficienciaRepository.java
+++ src/main/java/com/catalisa/redesolidaria/repository/TipoDaDeficienciaRepository.java
@@ -1,7 +1,9 @@
 package com.catalisa.redesolidaria.repository;
 
+import com.catalisa.redesolidaria.model.TipoDaDeficienciaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TipoDaDeficienciaRepository {
+public interface TipoDaDeficienciaRepository extends JpaRepository<TipoDaDeficienciaModel, Long> {
 }
--- src/main/java/com/catalisa/redesolidaria/repository/UsuarioRepository.java
+++ src/main/java/com/catalisa/redesolidaria/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package com.catalisa.redesolidaria.repository;
 
+import com.catalisa.redesolidaria.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository {
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
 }
