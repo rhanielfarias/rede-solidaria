8ae3175cb6a099c8bf177fc55effdbb5a2f68ae4
rhanielfarias <rhanielfarias@gmail.com>
2022-10-19 18:25:42 -0300

    Criando método de validar se é menor de idade o usuário

--- src/main/java/com/catalisa/redesolidaria/controller/UsuarioController.java
+++ src/main/java/com/catalisa/redesolidaria/controller/UsuarioController.java
@@ -1,7 +1,24 @@
 package com.catalisa.redesolidaria.controller;
 
+import com.catalisa.redesolidaria.model.UsuarioModel;
+import com.catalisa.redesolidaria.model.dto.UsuarioDtoResponse;
+import com.catalisa.redesolidaria.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
+@Autowired
+    private UsuarioService usuarioService;
+
+
+    @PostMapping(path = "/usuario/create")
+    public ResponseEntity<UsuarioDtoResponse> cadastrarUsuario(@RequestBody UsuarioModel usuarioModel) {
+        UsuarioDtoResponse usuario = usuarioService.cadastrar(usuarioModel);
+        return new ResponseEntity<>(usuario, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/catalisa/redesolidaria/service/UsuarioService.java
+++ src/main/java/com/catalisa/redesolidaria/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.catalisa.redesolidaria.service;
 
+import ch.qos.logback.core.rolling.helper.RollingCalendar;
 import com.catalisa.redesolidaria.model.UsuarioModel;
 import com.catalisa.redesolidaria.model.dto.UsuarioDtoResponse;
     import com.catalisa.redesolidaria.repository.UsuarioRepository;
@@ -7,23 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+UsuarioModel usuarioModel;
 
+    public boolean validadorDeMenorDeIdade(UsuarioModel usuarioModel) {
+        LocalDate data = usuarioModel.getDataDeNascimento().plusYears(18);
+        LocalDate now = LocalDate.now();
+        return data.isBefore(now);
+    }
     public UsuarioDtoResponse cadastrar(UsuarioModel usuarioModel) {
+Boolean validandoIdade = validadorDeMenorDeIdade(usuarioModel);
+if (validandoIdade) {
+    usuarioRepository.save(usuarioModel);
+    UsuarioDtoResponse usuarioDtoResponse = new UsuarioDtoResponse(usuarioModel.getId()
+            , usuarioModel.getCategoria(), usuarioModel.getNome(),
+            usuarioModel.getEmail(), usuarioModel.getLatitude(),
+            usuarioModel.getLongitude());
 
-        usuarioRepository.save(usuarioModel);
-
-        UsuarioDtoResponse usuarioDtoResponse = new UsuarioDtoResponse(usuarioModel.getId()
-                ,usuarioModel.getCategoria(),usuarioModel.getNome(),
-                usuarioModel.getEmail(),usuarioModel.getLatitude(),
-                usuarioModel.getLongitude());
-
-        return usuarioDtoResponse;
-
+    return usuarioDtoResponse;
+} else  {
+    return  null;
+}
     }
 
     private UsuarioDtoResponse atualizar(UsuarioModel usuarioModel, Long id){
