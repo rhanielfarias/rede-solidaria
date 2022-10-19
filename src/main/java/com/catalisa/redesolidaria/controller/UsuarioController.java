4ba1a363b6f57dc8582244d85de9511577100b8d
luizfernando03 <luizfernandodemoura.2013@gmail.com>
2022-10-19 15:11:34 -0300

    adicionando pasta

--- src/main/java/com/catalisa/redesolidaria/controller/UsuarioController.java
+++ src/main/java/com/catalisa/redesolidaria/controller/UsuarioController.java
@@ -1,7 +1,22 @@
 package com.catalisa.redesolidaria.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.catalisa.redesolidaria.model.UsuarioModel;
+import com.catalisa.redesolidaria.model.dto.UsuarioDtoResponse;
+import com.catalisa.redesolidaria.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<UsuarioDtoResponse> alterar(UsuarioModel usuarioModel){
+       UsuarioDtoResponse salvar = usuarioService.cadastrar (usuarioModel);
+       return ResponseEntity.status (HttpStatus.OK).body (salvar);
+    }
 }
